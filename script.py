e845691219fcaf007ae4f056c07238f611a1c204
paulduchesne <paulduchesne@tuta.io>
2024-09-15 21:58:21 +0200

    llm section

--- script.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-from clairmeta import DCP
-
-dcp = DCP(pathlib.Path.cwd() / 'test_dcp' / 'TestDcp_SHR-1_F-178_XX-XX_MOS_2K_20240914_SMPTE_OV')
-dcp.parse()
-status, report = dcp.check()
-
-
-    #   TestDcp_SHR-1_F-178_XX-XX_MOS_2K_20240914_SMPTE_OV/cpl_1c6ad489-fa93-4ddb-9cea-5a1ce6f71e0e.xml
